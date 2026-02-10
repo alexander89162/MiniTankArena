26374342995671c0c0fd2b08f0de08cdc29e29f8
Lgranad13 <leogranadoscastillo13@gmail.com>
2026-02-10 10:28:53 -0800

    Basic Movement, Basic Third person view, Wheel Colliders to go up terrain

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    public float turnSpeed = 100f;
+
+    Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Vertical") * speed;
+        float turn = Input.GetAxis("Horizontal") * turnSpeed;
+
+        Vector3 forwardMove = transform.forward * move * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + forwardMove);
+
+        Quaternion turnRot = Quaternion.Euler(0f, turn * Time.fixedDeltaTime, 0f);
+        rb.MoveRotation(rb.rotation * turnRot);
+    }
+}
