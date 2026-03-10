0d9c0a5eec0b49ebae773cf941c3b92f3c4da162
Alexander <alexander891622gmail.com>
2026-03-10 12:25:36 -0700

    Added an arrow prefab we can use to visualize flight paths for the drone, deleted DroneFlightTest scene, added validation method to DroneController

--- Assets/Scripts/TestMiscellaneous/DroneController.cs
+++ Assets/Scripts/TestMiscellaneous/DroneController.cs
@@ -1,49 +1,71 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UIElements;
 
-/*Given a series of positions along a path, this component automatically 
-makes the drone move along the path and adjust speed as needed. It also
-animates the drone automatically based on the specified movesQueue in a JSON 
-(or hard-coded test case)*/
+/*Given a series of Move instructions, this component makes the drone move along 
+the path and interpolate values as needed to animate the drone automatically*/
 public class DroneController : MonoBehaviour
 {
-    public string dronePath; // the file containing the specific path this drone will follow
-    public string dronePathDirectory; // directory that contains dronePath file
+    public string droneActions; // the file containing the specific actions this drone will follow
+    public string droneActionsPath; // directory that contains droneActions file
 
-    private Queue<Move> movesQueue;
-    private Vector3 nextPos; // next "checkpoint" to reach
+    private List<Move> moves;
+    private List<BrakingManeuver> brakingManeuvers;
+    private List<DeploymentAction> deploymentActions;
     private int currentNodeIndex = 0;
-    private Vector3 move;
+    AnimationState currentState = AnimationState.Forward;
 
     private enum AnimationState
     {
+        InitializingController, // not ready to move or animate yet
         Forward, // leans the drone forward and tilts based on next path node
         StabilizingFromStop, // rock back and forth to simulate realistic stopping
         Idle, // Rock up and down, small sideways noise which should always return to original position and tilt gently while doing so
     }
 
-    AnimationState currentState = AnimationState.Forward;
-    private Quaternion defaultRotation; // we use this to remember the forward-facing direction
-
-    private struct Move
+    public class DroneActions
     {
-        readonly int moveId;
-        readonly Vector3 startPos;
-        readonly Vector3 endPos;
-        readonly float startVelocity; // velocity when at this node
-        readonly float endVelocity; // velocity by the time of reaching the next node
-        readonly string accelerationType; // "linear" or "quadratic"
-        Quaternion targetRotation; // the drone should be mostly facing in this direction
-        readonly string rotationType; // "linear" or "Slerp"
+        public readonly Move[] moves;
+        public readonly BrakingManeuver[] brakingManeuvers;
+        public readonly DeploymentAction[] deploymentActions;
     }
+
+    public struct Move
+    {
+        public readonly int moveId;
+        public readonly Vector3 position;
+        public readonly Quaternion rotation;
+        public readonly float endVelocity;
+        public readonly string accelerationType; // "linear" or "quadratic"
+        public readonly string rotationType; // "linear" or "Slerp"
+    }
+
+    public readonly struct BrakingManeuver
+    {
+        public readonly Quaternion rotation;
+        public readonly float duration;
+        public readonly float outwardMove;
+    }
+
+    public readonly struct DeploymentAction
+    {
+        public readonly string action;
+        public readonly int activationNode;
+        public readonly float startDelay;
+        public readonly float duration;
+    }
+
+    // Allowed interpolation methods
+    private static readonly HashSet<string> allowedAccelerationTypes = new HashSet<string>{ "linear", "quadratic" };
+    private static readonly HashSet<string> allowedRotationTypes = new HashSet<string>{ "linear", "slerp" };
 
     void Awake()
     {
-        defaultRotation = transform.localRotation;
-        LoadDronePath();
-        BuildMovesQueue();
+        SetState(AnimationState.InitializingController);
+        StartCoroutine(InitializeDroneActions());
     }
 
     void Update()
@@ -59,21 +81,63 @@ public class DroneController : MonoBehaviour
         }
     }
 
-    private IEnumerator LoadDronePath()
+    private IEnumerator InitializeDroneActions()
     /*Extract drone path data from JSON*/
     {
-        string path = System.IO.Path.Combine(dronePathDirectory, System.String.Concat(dronePath, ".json"));
+        // 1) Extract data from json
+        string path = System.IO.Path.Combine(droneActionsPath, System.String.Concat(droneActions, ".json"));
 
         UnityWebRequest request = UnityWebRequest.Get(path);
-        yield return null;
+        yield return request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError("Failed to load drone actions: " + request.error);
+            yield break;
+        }
+
+        DroneActions actions =
+            JsonUtility.FromJson<DroneActions>(
+                request.downloadHandler.text
+            );
+
+        // 2) Validation - fail = delete this drone to avoid crashes
+        if (!ValidateDroneActions(actions))
+        {
+            Debug.LogError($"DroneController on {name} received invalid action data. Destroying drone.");
+            Destroy(gameObject);
+            yield break;
+        }
+
+        // 3) Cache the data we need
+        moves = new List<Move>(actions.moves);
+        brakingManeuvers = new List<BrakingManeuver>(actions.brakingManeuvers);
+        deploymentActions = new List<DeploymentAction>(actions.deploymentActions);
+
+        // 4) Done initializing
+        SetState(AnimationState.Forward);
     }
 
-    public void BuildMovesQueue()
-    /*Use the given pathNodes to compute each move the drone should undergo, then
-    put the moves into the movesQueue*/
+    private bool ValidateDroneActions(DroneActions actions)
     {
-        // 1) Read values from JSON
+        // 1) Interpolation methods must be valid
+        for (int i = 0; i < moves.Count; i++)
+        {
+            var move = moves[i];
+            if (string.IsNullOrEmpty(move.accelerationType) || !allowedAccelerationTypes.Contains(move.accelerationType.ToLower()))
+            {
+                Debug.LogError($"Acceleration type is invalid or missing");
+                return false;   
+            }
+            if (string.IsNullOrEmpty(move.rotationType) || !allowedRotationTypes.Contains(move.rotationType.ToLower()))
+            {
+                Debug.LogError($"Rotation type is invalid or missing");
+                return false;   
+            }
+        }
 
-        // 2) Fill movesQueue
+        return true;
     }
+
+    private void SetState(AnimationState newState){ currentState = newState; }
 }
